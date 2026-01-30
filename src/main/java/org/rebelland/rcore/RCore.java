0cbf2dd47d37c2b5d331f0ad1f1b20bc01d72cd6
MakarME <brawleraid99@gmail.com>
2026-01-31 00:48:08 +0200

    feat: new API registration and PlaytimeService handling

--- src/main/java/org/rebelland/rcore/RCore.java
+++ src/main/java/org/rebelland/rcore/RCore.java
@@ -3,13 +3,16 @@ package org.rebelland.rcore;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mineacademy.fo.Common;
 import org.mineacademy.fo.plugin.SimplePlugin;
+import org.rebelland.rcore.api.AntiSocialAPI;
 import org.rebelland.rcore.api.BoostAPI;
+import org.rebelland.rcore.api.CheaterStatusAPI;
 import org.rebelland.rcore.api.ClansAPI;
 import org.rebelland.rcore.database.DatabaseService;
 import org.rebelland.rcore.database.UserRepository;
 import org.rebelland.rcore.listener.GlobalPlayerManager;
 import org.rebelland.rcore.listener.UserListener;
 import org.rebelland.rcore.redis.RedisService;
+import org.rebelland.rcore.service.PlaytimeService;
 
 public final class RCore extends SimplePlugin {
 
@@ -17,6 +20,8 @@ public final class RCore extends SimplePlugin {
     private RedisService redisService;
     private static BoostAPI boostAPI;
     private static ClansAPI clansAPI;
+    private static CheaterStatusAPI cheaterStatusAPI;
+    private static AntiSocialAPI antiSocialAPI;
 
     @Override
     public void onPluginStart() {
@@ -46,10 +51,12 @@ public final class RCore extends SimplePlugin {
 
         registerEvents(new UserListener());
         registerEvents(new GlobalPlayerManager());
+        PlaytimeService.getInstance();
     }
 
     @Override
     public void onPluginStop() {
+        PlaytimeService.getInstance().shutdown();
         if (redisService != null) redisService.shutdown();
         if (databaseService != null) databaseService.shutdown();
     }
@@ -70,6 +77,24 @@ public final class RCore extends SimplePlugin {
 
     public static ClansAPI getClansAPI() {
         return clansAPI;
+    }
+
+    public static void setCheaterStatusAPI(CheaterStatusAPI api) {
+        Common.log("CheaterStatusAPI инициализирован");
+        cheaterStatusAPI = api;
+    }
+
+    public static CheaterStatusAPI getCheaterStatusAPI() {
+        return cheaterStatusAPI;
+    }
+
+    public static void setAntiSocialAPI(AntiSocialAPI api) {
+        Common.log("AntiSocialAPI инициализирован");
+        antiSocialAPI = api;
+    }
+
+    public static AntiSocialAPI getAntiSocialAPI() {
+        return antiSocialAPI;
     }
 
     public static RCore getInstance() {
