144fa8a71fa616e35f6f0c31e280b89fe765b7ad
MakarME <brawleraid99@gmail.com>
2026-02-01 20:37:50 +0200

    feat: new AntiSocialAPI methods

--- src/main/java/org/rebelland/rcore/api/AntiSocialAPI.java
+++ src/main/java/org/rebelland/rcore/api/AntiSocialAPI.java
@@ -1,6 +1,6 @@
 package org.rebelland.rcore.api;
 
-import org.rebelland.rcore.model.socials.Friend;
+import org.rebelland.rcore.model.socials.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -8,9 +8,29 @@ import java.util.UUID;
 public interface AntiSocialAPI {
     long getReputation(UUID uuid);
 
-    boolean arePingsEnabled(UUID uuid);
+    PingMode getPingMode(UUID uuid);
+
+    boolean canPing(UUID sender, UUID target);
 
     boolean isBlocked(UUID owner, UUID target);
 
-    public List<Friend> getFriends(UUID uuid);
+    List<Friend> getFriends(UUID uuid);
+
+    public BlockedPlayer getBlockEntry(UUID sender, UUID target);
+
+    /**
+     * Можно ли Sender'у отправить запрос на телепортацию (TPA) к Target'у?
+     */
+    public boolean canTpa(UUID sender, UUID target);
+
+    /**
+     * Можно ли Sender'у написать в ЛС Target'у?
+     */
+    public boolean canSendPm(UUID sender, UUID target);
+
+    /**
+     * Видит ли Target сообщения Sender'а в глобальном чате?
+     * (Используйте это в ивенте чата: event.getRecipients().removeIf...)
+     */
+    boolean canSeeChat(UUID sender, UUID target);
 }
